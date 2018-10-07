e4695635f4084766f84dc052ddf82bd34c4cab67
jsw76 <jonathansw@outlook.com>
2018-10-06 18:21:46 -0700

    allow for local or heroku deploy

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ bot.on('ready', () => {
 
 bot.on('message', async (message) => {
   if (message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
+  if(message.content.indexOf(process.env.PREFIX || config.prefix) !== 0) return;
 
   let command = message.content.slice(1).toLowerCase();
   let response = [];
@@ -46,4 +46,4 @@ bot.on('message', async (message) => {
   }
 });
 
-bot.login(config.token);
+bot.login(process.env.TOKEN || config.token);
